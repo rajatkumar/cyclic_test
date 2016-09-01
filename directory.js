7a9198db8ca08bf830cb737dc0df430802a09e36
rajatk <rajatk@netflix.com>
2016-09-01 15:03:11 -0700

    Added readme and codex-output.json

--- directory.js
+++ directory.js
@@ -1,4 +1,4 @@
-var local = "";
+var local = ""; //used for forcing codexjs to not traverse this
 var directory = {
         'classA': function(){ return require(local+'./classA')},
         'classB': function(){ return require(local+'./classB')},
